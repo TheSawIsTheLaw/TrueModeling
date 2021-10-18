ce7617497511cf1c0a668fb2b4b24a12ac566a63
trvehazzk3r <dimakrok@bk.ru>
2021-10-18 19:14:08 +0300

    ADD: sample of kotlin's swing

---
+++ lab1/src/main.kt
@@ -0,0 +1,32 @@
+import java.awt.EventQueue
+import javax.swing.*
+
+class KotlinSwingSimpleEx(title: String) : JFrame()
+{
+    init
+    {
+        createUI(title)
+    }
+
+    private fun createUI(title: String)
+    {
+
+        setTitle(title)
+
+        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        setSize(300, 200)
+        setLocationRelativeTo(null)
+    }
+}
+
+private fun createAndShowGUI()
+{
+
+    val frame = KotlinSwingSimpleEx("Simple")
+    frame.isVisible = true
+}
+
+fun main()
+{
+    EventQueue.invokeLater(::createAndShowGUI)
+}
