49558ed0ba66ec5603bfc247e5c4e4f54dcd31fb
trvehazzk3r <dimakrok@bk.ru>
2021-11-10 15:18:59 +0300

    STO: resizes

--- lab2/src/ModelingLab02/mainwindow.cpp
+++ lab2/src/ModelingLab02/mainwindow.cpp
@@ -1,11 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    this->setWindowTitle("Лабораторная работа 2, Якуба, ИУ7-73Б");
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +13,42 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::resizeIntensityMatrix(int size)
+{
+    intensityMatrix.resize(size);
+    for (auto &row : intensityMatrix)
+    {
+        row.resize(size);
+    }
+}
+
+void MainWindow::resizeTableWidget(QTableWidget *tableWidget, int rows, int cols)
+{
+    tableWidget->setRowCount(rows);
+    tableWidget->setColumnCount(cols);
+
+    for (int i = 0; i < rows; ++i)
+    {
+        for (int j = 0; j < cols; ++j)
+        {
+            if (!tableWidget->item(i, j))
+            {
+                tableWidget->setItem(i, j, new QTableWidgetItem);
+            }
+        }
+    }
+}
+
+void MainWindow::on_numberOfStatesSpinBox_textChanged(const QString &arg1)
+{
+    int currentStatesNumber = ui->numberOfStatesSpinBox->value();
+
+    resizeTableWidget(ui->intensityMatrixTableWidget, currentStatesNumber, currentStatesNumber);
+    resizeTableWidget(ui->resultTableWidget, currentStatesNumber, 3);
+
+    ui->resultTableWidget->setHorizontalHeaderLabels(QStringList() << "Вероятность"
+                                                                   << "t1"
+                                                                   << "t2");
+
+    resizeIntensityMatrix(currentStatesNumber);
+}
--- lab2/src/ModelingLab02/mainwindow.h
+++ lab2/src/ModelingLab02/mainwindow.h
@@ -2,20 +2,35 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
+#include <QStringList>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
+  public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private:
+  private slots:
+    void on_numberOfStatesSpinBox_textChanged(const QString &arg1);
+
+  private:
     Ui::MainWindow *ui;
+
+    QVector<QVector<double>> intensityMatrix;
+
+    void resizeIntensityMatrix(int size);
+    void resizeTableWidget(QTableWidget *tableWidget, int rows, int cols);
+    void resizeIntensityMatrixTableWidget(int size);
+    void resizeResultTableWidget(int size);
 };
 #endif // MAINWINDOW_H
