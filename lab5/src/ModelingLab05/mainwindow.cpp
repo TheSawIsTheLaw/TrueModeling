34a85036377dd308e5f1812d6ecc5f6ef951cba3
TheSawIsTheLaw <dimakrok@bk.ru>
2021-12-04 16:21:09 +0300

    ADD: starts ui

---
+++ lab5/src/ModelingLab05/mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.hpp"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Якуба, ИУ7-73Б, Лабораторная работа 5");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
